ce081a33611f0723c7c9f11d36c054b02b400656
Ardak Abdullakhan <sjwiggrbrkwoscbejwi@gmail.com>
2024-01-27 10:16:57 +0600

    added tuple examples

---
+++ lab2/Examples/tuple/tuples/tup4.py
@@ -0,0 +1,6 @@
+thistuple = ("apple",)#remember the comma
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple)) 
---
+++ lab2/Examples/tuple/tuples/tup6.py
@@ -0,0 +1,3 @@
+tuple1 = ("abc", 34, True, 40, "male")#contain diff data types
+
+print(tuple1)
