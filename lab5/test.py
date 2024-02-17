70ad61133582f7c2e0f0e178a8b678ed58326cf7
Ardak Abdullakhan <sjwiggrbrkwoscbejwi@gmail.com>
2024-02-17 18:18:01 +0600

    added some changes

--- lab5/test.py
+++ lab5/test.py
@@ -1,19 +1,4 @@
 import re
 
-def snake_to_camel(snake_str):
-    # Use regex to split the snake case string into words
-    words = snake_str.split('_')
-    
-    # Capitalize the first letter of each word except the first one
-    camel_words = [words[0].lower()] + [word.capitalize() for word in words[1:]]
-    
-    # Join the words to form the camel case string
-    camel_str = ''.join(camel_words)
-    
-    return camel_str
-
-# Test the function
-snake_case_string = "snake_case_example_string"
-camel_case_string = snake_to_camel(snake_case_string)
-print("Snake case string:", snake_case_string)
-print("Camel case string:", camel_case_string)
+txt = "vhvcnwlcicjfvmabbbbbbfmivmwcl"
+print(re.search("a{1}b*",txt))
